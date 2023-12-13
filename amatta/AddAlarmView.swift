3ec3aca9027fc0201871a6bc3fc7f9afb528618c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-13 17:55:57 +0900

    datepicker 커스텀 못하나?

--- amatta/AddAlarmView.swift
+++ amatta/AddAlarmView.swift
@@ -9,13 +9,48 @@ import Foundation
 import SwiftUI
 
 struct AddAlarmView: View {
+    @State private var alarmName: String = ""
+    @State private var selectedTime = Date()
+    @State private var selectedWeekdays: [Bool] = Array(repeating: false, count: 7)
+
     var body: some View {
-        VStack {
-            Text("알림 추가")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding()
-            Spacer()
+        NavigationView {
+            Form {
+                // 알림 이름 입력
+                Section(header: Text("알림 이름")) {
+                    TextField("알림 이름을 입력해주세요.", text: $alarmName)
+                }
+
+                // 알림 시간 설정
+                Section(header: Text("알림 시간")) {
+                    DatePicker("", selection: $selectedTime, displayedComponents: .hourAndMinute)
+                }
+
+                // 요일 선택
+                Section(header: Text("요일 선택")) {
+                    Toggle("월", isOn: $selectedWeekdays[0])
+                    Toggle("화", isOn: $selectedWeekdays[1])
+                    Toggle("수", isOn: $selectedWeekdays[2])
+                    Toggle("목", isOn: $selectedWeekdays[3])
+                    Toggle("금", isOn: $selectedWeekdays[4])
+                    Toggle("토", isOn: $selectedWeekdays[5])
+                    Toggle("일", isOn: $selectedWeekdays[6])
+                }
+
+                // 소지품 추가
+                Section {
+                    Button(action: {
+                        // 소지품 추가 기능
+                    }) {
+                        HStack {
+                            Image(systemName: "plus")
+                            Text("여기를 눌러 소지품 추가")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("알림 추가")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
