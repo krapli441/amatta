5edf26e2e548a3095bb4809c9c9f632c2af574fd
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-14 12:09:10 +0900

    뒤로 가기 버튼을 '뒤로' 로 바꾸고 싶음

---
+++ amatta/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  amatta
+//
+//  Created by 박준형 on 12/14/23.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // 여기에 글로벌 설정을 추가.
+//        UINavigationBar.appearance().backButtonTitle = "뒤로"
+        return true
+    }
+}
