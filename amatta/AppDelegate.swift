738a4094710d03ec6cd1a4e9119161643e3cb9b0
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-16 16:54:16 +0900

    CoreData 인식 성공, 매인 앱 파일에서 해줘야 했음

--- amatta/AppDelegate.swift
+++ amatta/AppDelegate.swift
@@ -1,59 +1,59 @@
+////
+////  AppDelegate.swift
+////  amatta
+////
+////  Created by 박준형 on 12/14/23.
+////
 //
-//  AppDelegate.swift
-//  amatta
+//import Foundation
+//import UIKit
+//import CoreData
 //
-//  Created by 박준형 on 12/14/23.
+//class AppDelegate: NSObject, UIApplicationDelegate {
 //
-
-import Foundation
-import UIKit
-import CoreData
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        // CoreData 초기화 코드
-        self.persistentContainer.loadPersistentStores { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        }
-        
-        // CoreData 모델 검증 코드
-        let persistentContainer = self.persistentContainer
-        let managedObjectModel = persistentContainer.managedObjectModel
-        let entities = managedObjectModel.entities
-
-        for entity in entities {
-            print("Entity name: \(entity.name ?? "Unknown")")
-            // 여기에서 추가적인 검증 로직을 구현할 수 있습니다.
-        }
-        
-        return true
-    }
-
-    // MARK: - Core Data stack
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "AlarmData")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-}
-
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        // CoreData 초기화 코드
+//        self.persistentContainer.loadPersistentStores { (storeDescription, error) in
+//            if let error = error as NSError? {
+//                fatalError("Unresolved error \(error), \(error.userInfo)")
+//            }
+//        }
+//        
+//        // CoreData 모델 검증 코드
+//        let persistentContainer = self.persistentContainer
+//        let managedObjectModel = persistentContainer.managedObjectModel
+//        let entities = managedObjectModel.entities
+//
+//        for entity in entities {
+//            print("Entity name: \(entity.name ?? "Unknown")")
+//            // 여기에서 추가적인 검증 로직을 구현할 수 있습니다.
+//        }
+//        
+//        return true
+//    }
+//
+//    // MARK: - Core Data stack
+//    lazy var persistentContainer: NSPersistentContainer = {
+//        let container = NSPersistentContainer(name: "AlarmData")
+//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+//            if let error = error as NSError? {
+//                fatalError("Unresolved error \(error), \(error.userInfo)")
+//            }
+//        })
+//        return container
+//    }()
+//
+//    // MARK: - Core Data Saving support
+//    func saveContext () {
+//        let context = persistentContainer.viewContext
+//        if context.hasChanges {
+//            do {
+//                try context.save()
+//            } catch {
+//                let nserror = error as NSError
+//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//            }
+//        }
+//    }
+//}
+//
--- amatta/amattaApp.swift
+++ amatta/amattaApp.swift
@@ -7,12 +7,36 @@
 
 import SwiftUI
 import SwiftData
+import CoreData
 
 @main
 struct amattaApp: App {
+    let persistentContainer: NSPersistentContainer
+
+    init() {
+        // 여기에서 CoreData 모델 파일 이름을 프로젝트에 맞게 수정하세요.
+        persistentContainer = NSPersistentContainer(name: "AlarmData")
+
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+
+        // CoreData 모델 검증 코드
+        let entities = persistentContainer.managedObjectModel.entities
+        for entity in entities {
+            print("Entity name: \(entity.name ?? "Unknown")")
+            // 추가적인 검증 로직을 여기에 구현할 수 있습니다.
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                // managedObjectContext를 ContentView에 전달합니다.
+                .environment(\.managedObjectContext, persistentContainer.viewContext)
         }
     }
 }
+
