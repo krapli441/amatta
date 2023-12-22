9f3f12da85bc3293277a5eead9835a92cee0641f
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-22 15:07:45 +0900

    먼저 챙겨야 할 것들 목록을 버튼 형식으로 수정

--- amatta/EditAlarmView.swift
+++ amatta/EditAlarmView.swift
@@ -126,36 +126,39 @@ struct EditAlarmView: View {
         VStack(alignment: .center, spacing: 5) {
             SectionHeaderView(title: "챙겨야 할 것들")
             ForEach(items, id: \.self) { item in
-                
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(item.name ?? "Unknown")
-                            .font(.headline)
-                            .foregroundColor(colorScheme == .dark ? .white : .black)
-
-                        if let children = item.children as? Set<Items>, !children.isEmpty {
-                            Text(children.map { $0.name ?? "" }.joined(separator: ", "))
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
+                Button(action: {
+                    // 여기에 특정 item을 편집하는 로직을 추가
+                    // 예: EditItemView로의 이동
+                }) {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name ?? "Unknown")
+                                .font(.headline)
+                                .foregroundColor(colorScheme == .dark ? .white : .black)
+                            
+                            if let children = item.children as? Set<Items>, !children.isEmpty {
+                                Text(children.map { $0.name ?? "" }.joined(separator: ", "))
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
                         }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
                     }
-                    Spacer()
-                    Image(systemName: "chevron.right")
-                        .foregroundColor(.gray)
+                    .padding()
+                    .frame(maxWidth: 350, alignment: .leading)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                    // 편집 모드나 추가 로직은 필요한 경우 구현
                 }
-                .padding()
-                .frame(maxWidth: 350, alignment: .leading)
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
-                // 편집 모드나 추가 로직은 필요한 경우 구현
             }
         }
     }
-
 
 
     private func formatContainedItems(_ items: [String]) -> String {
