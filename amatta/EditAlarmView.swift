d5d320f7ee2d2758a2117d06d29414266bc7d786
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-26 18:10:13 +0900

    이전 브랜치는 변경사항이 잘 감지되는데. 대체 왜?

--- amatta/EditAlarmView.swift
+++ amatta/EditAlarmView.swift
@@ -239,55 +239,32 @@ struct EditAlarmView: View {
     }
     
     private func saveAlarm() {
-        
-        let calendar = Calendar.current
-        let currentDateComponents = calendar.dateComponents([.year, .month, .day], from: Date())
-        var timeComponents = calendar.dateComponents([.hour, .minute], from: selectedTime)
-        timeComponents.year = currentDateComponents.year
-        timeComponents.month = currentDateComponents.month
-        timeComponents.day = currentDateComponents.day
-        timeComponents.second = 0
-        
-        let newAlarm = Alarm(context: managedObjectContext)
-        newAlarm.name = alarmName
-        newAlarm.alarmIdentifier = UUID().uuidString
-        newAlarm.time = calendar.date(from: timeComponents)
-        newAlarm.sunday = selectedWeekdays[0] // 일요일
-        newAlarm.monday = selectedWeekdays[1] // 월요일
-        newAlarm.tuesday = selectedWeekdays[2] // 화요일
-        newAlarm.wednesday = selectedWeekdays[3] // 수요일
-        newAlarm.thursday = selectedWeekdays[4] // 목요일
-        newAlarm.friday = selectedWeekdays[5] // 금요일
-        newAlarm.saturday = selectedWeekdays[6] // 토요일
-
-
-        for temporaryItem in alarmCreationData.items {
-            let newItem = Items(context: managedObjectContext)
-            newItem.name = temporaryItem.name
-            newItem.isContainer = temporaryItem.isContainer
-            newItem.importance = temporaryItem.importance
-
-            if temporaryItem.isContainer {
-                for childName in temporaryItem.containedItems {
-                    let childItem = Items(context: managedObjectContext)
-                    childItem.name = childName
-                    childItem.isContainer = false // 자식 아이템은 컨테이너가 아님
-                    newItem.addToChildren(childItem)
-                }
-            }
-
-            newAlarm.addToItems(newItem)
+        guard let alarmID = alarmID, let alarm = managedObjectContext.object(with: alarmID) as? Alarm else {
+            print("Alarm not found")
+            return
         }
 
+        // 알람 정보 업데이트
+        alarm.name = alarmName
+        alarm.time = selectedTime
+        alarm.sunday = selectedWeekdays[0]
+        alarm.monday = selectedWeekdays[1]
+        alarm.tuesday = selectedWeekdays[2]
+        alarm.wednesday = selectedWeekdays[3]
+        alarm.thursday = selectedWeekdays[4]
+        alarm.friday = selectedWeekdays[5]
+        alarm.saturday = selectedWeekdays[6]
 
-        
+        // 기존 아이템을 삭제하거나 업데이트
+//        updateItems(for: alarm, with: alarmCreationData.items)
+
+        // CoreData 저장
         do {
             try managedObjectContext.save()
-            print("알람 저장 성공: \(newAlarm)")
-            NotificationManager.shared.scheduleNotification(for: newAlarm)
+            print("알람 업데이트 성공")
             presentationMode.wrappedValue.dismiss()
         } catch let error as NSError {
-            print("알람 저장 실패: \(error), \(error.userInfo)")
+            print("알람 업데이트 실패: \(error), \(error.userInfo)")
         }
     }
 
