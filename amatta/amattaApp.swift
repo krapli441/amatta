b896ceaa8f723c02f59f3bdab6571e4c25f3bf9b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-27 16:12:32 +0900

    마이그레이션 됐는지 점검 필요

--- amatta/amattaApp.swift
+++ amatta/amattaApp.swift
@@ -14,14 +14,19 @@ struct amattaApp: App {
     let persistentContainer: NSPersistentContainer
 
     init() {
-        // 여기에서 CoreData 모델 파일 이름을 프로젝트에 맞게 수정하세요.
         persistentContainer = NSPersistentContainer(name: "AlarmData")
 
-        persistentContainer.loadPersistentStores { storeDescription, error in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        }
+           let description = NSPersistentStoreDescription()
+           description.shouldMigrateStoreAutomatically = true
+           description.shouldInferMappingModelAutomatically = true
+
+           persistentContainer.persistentStoreDescriptions = [description]
+
+           persistentContainer.loadPersistentStores { storeDescription, error in
+               if let error = error as NSError? {
+                   fatalError("Unresolved error \(error), \(error.userInfo)")
+               }
+           }
 
         // CoreData 모델 검증 코드
         let entities = persistentContainer.managedObjectModel.entities
@@ -34,7 +39,6 @@ struct amattaApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                // managedObjectContext를 ContentView에 전달합니다.
                 .environment(\.managedObjectContext, persistentContainer.viewContext)
         }
     }
