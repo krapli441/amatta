04dc50c0df236616706db6e48d12de1206378845
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-28 17:36:14 +0900

    아이템 정보를 불러오는데에 성공함

--- amatta/AlarmEditModifyItemView.swift
+++ amatta/AlarmEditModifyItemView.swift
@@ -13,27 +13,54 @@ struct AlarmEditModifyItemView: View {
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.presentationMode) var presentationMode
     @Environment(\.managedObjectContext) private var managedObjectContext
-    
-    // selectedItemObjectID 추가
     @Binding var itemObjectID: NSManagedObjectID?
-    
-    // 이니셜라이저를 추가하여 selectedItemObjectID를 설정
+
+    @State private var itemDetails: String = "Loading..."
+
     init(itemObjectID: Binding<NSManagedObjectID?>) {
-            self._itemObjectID = itemObjectID
-        }
-    
+        self._itemObjectID = itemObjectID
+    }
+
     var body: some View {
         VStack {
             EditItemHeaderView() // 물건 변경
             ScrollView {
                 Text("Selected Item ObjectID: \(itemObjectID?.description ?? "None")")
-                    .onAppear {
-                        print("Displaying in AlarmEditModifyItemView: \(String(describing: itemObjectID))")
-                    }
+                Text(itemDetails) // 여기에 아이템 세부 정보를 표시
             }
-
+            .onAppear {
+                loadItemDetails()
+            }
         }
     }
+
+    private func loadItemDetails() {
+        guard let objectID = itemObjectID, let item = managedObjectContext.object(with: objectID) as? Items else {
+            itemDetails = "Item not found"
+            return
+        }
+
+        let itemName = item.name ?? "Unknown"
+        let itemImportance = item.importance
+        let isContainer = item.isContainer ? "Yes" : "No"
+
+        // 기본 아이템 정보
+        itemDetails = "Name: \(itemName)\nImportance: \(itemImportance)\nIs Container: \(isContainer)"
+
+        // 컨테이너일 경우 하위 아이템 정보 추가
+        if item.isContainer, let children = item.children as? Set<Items>, !children.isEmpty {
+            let childItemsDetails = children
+                .sorted { ($0.creationDate ?? Date.distantPast) < ($1.creationDate ?? Date.distantPast) }
+                .map { child in
+                    "Child Item: \(child.name ?? "Unknown"), Creation Date: \(child.creationDate ?? Date()), ObjectID: \(child.objectID)"
+                }
+                .joined(separator: "\n")
+            
+            itemDetails += "\n\n\(childItemsDetails)"
+        }
+    }
+
+
 }
 
 struct AlarmEditModifyItemView_Previews: PreviewProvider {
@@ -41,4 +68,5 @@ struct AlarmEditModifyItemView_Previews: PreviewProvider {
         AlarmEditModifyItemView(itemObjectID: .constant(nil))
     }
 }
+
 
