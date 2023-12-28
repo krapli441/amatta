84653ba461da59d85c0ce3bae9a9b83532f7def0
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-28 17:58:52 +0900

    제거 버튼 추가 필요함

--- amatta/AlarmEditModifyItemView.swift
+++ amatta/AlarmEditModifyItemView.swift
@@ -76,35 +76,18 @@ struct AlarmEditModifyItemView: View {
 
 
     private func loadItemDetails() {
-        guard let objectID = itemObjectID, let item = managedObjectContext.object(with: objectID) as? Items else {
-            itemDetails = "Item not found"
-            return
+            guard let objectID = itemObjectID, let item = managedObjectContext.object(with: objectID) as? Items else {
+                return
+            }
+
+            itemName = item.name ?? "Unknown"
+            canContainOtherItems = item.isContainer
+            importance = item.importance
+
+            if let children = item.children as? Set<Items>, !children.isEmpty {
+                containedItems = children.map { $0.name ?? "Unknown" }
+            }
         }
-
-        // @State 변수 itemName에 아이템 이름 할당
-        itemName = item.name ?? "Unknown"
-
-        let itemImportance = item.importance
-        let isContainer = item.isContainer ? "Yes" : "No"
-
-        // 기본 아이템 정보
-        itemDetails = "Importance: \(itemImportance)\nIs Container: \(isContainer)"
-
-        // 컨테이너일 경우 하위 아이템 정보 추가
-        if item.isContainer, let children = item.children as? Set<Items>, !children.isEmpty {
-            let childItemsDetails = children
-                .sorted { ($0.creationDate ?? Date.distantPast) < ($1.creationDate ?? Date.distantPast) }
-                .map { child in
-                    "Child Item: \(child.name ?? "Unknown"), Creation Date: \(child.creationDate ?? Date()), ObjectID: \(child.objectID)"
-                }
-                .joined(separator: "\n")
-            
-            itemDetails += "\n\n\(childItemsDetails)"
-        }
-    }
-
-
-
 }
 
 struct AlarmEditModifyItemView_Previews: PreviewProvider {
