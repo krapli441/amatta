92ee9b1ee48bfa1eea7b89ce37b857fcd6ddb7c9
박준형 <108733795+krapli441@users.noreply.github.com>
2023-12-29 15:57:45 +0900

    알림 로직 수정

--- amatta/NotificationManager.swift
+++ amatta/NotificationManager.swift
@@ -15,27 +15,31 @@ class NotificationManager {
 
     func scheduleNotification(for alarm: Alarm) {
         guard let alarmName = alarm.name, let alarmTime = alarm.time else { return }
-        
+
         let content = UNMutableNotificationContent()
 
-        // Alarm의 items를 Items 타입의 배열로 변환
-            if let itemsSet = alarm.items as? Set<Items>, !itemsSet.isEmpty {
-                // 중요도가 가장 높은 물건을 찾음
-                let mostImportantItem = itemsSet.sorted(by: { $0.importance > $1.importance }).first
-
-                // 물건에 따라 제목과 본문을 다르게 설정
-                if let item = mostImportantItem {
-                    content.title = "혹시... \(item.name ?? "물건") 챙기셨나요?"
-                    content.body = "\(alarmName) 준비하는데 꼭 필요한 물건이에요!"
+        if let itemsSet = alarm.items as? Set<Items>, !itemsSet.isEmpty {
+            // 중요도가 가장 높은 물건을 무작위로 선택
+            let importantItems = itemsSet.filter { $0.importance == itemsSet.max(by: { $0.importance < $1.importance })?.importance }
+            if let randomItem = importantItems.randomElement() {
+                content.title = "혹시.. \(randomItem.name ?? "물건") 챙기셨나요?"
+                
+                // 하위 물건들을 포함하는 메시지 생성
+                if randomItem.isContainer, let children = randomItem.children as? Set<Items>, !children.isEmpty {
+                    let childrenNames = children.map { $0.name ?? "항목" }.joined(separator: ", ")
+                    content.body = "\(childrenNames), 잘 들어있는지 확인해보세요!"
                 } else {
-                    content.title = "\(alarmName) 준비 시간입니다!"
-                    content.body = "놓고 간 물건이 있는지 확인해볼까요?"
+                    content.body = "\(alarmName) 준비하는데 꼭 필요한 물건이에요!"
                 }
             } else {
                 content.title = "\(alarmName) 준비 시간입니다!"
-                content.body = "까먹은 건 없는지 확인해볼까요?"
+                content.body = "놓고 간 물건이 있는지 확인해볼까요?"
             }
-            content.sound = UNNotificationSound.default
+        } else {
+            content.title = "\(alarmName) 준비 시간입니다!"
+            content.body = "빼먹은 물건이 있는지 확인해볼까요?"
+        }
+        content.sound = UNNotificationSound.default
 
         let calendar = Calendar.current
         var dateComponents = calendar.dateComponents([.hour, .minute], from: alarmTime)
