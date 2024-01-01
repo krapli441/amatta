76f50ea48274d4b807f8ec93a21212337edf9efb
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-01 14:34:30 +0900

    푸시 알람에 사용할 alarmmodel 추가

---
+++ amatta/PushAlarmModel.swift
@@ -0,0 +1,20 @@
+//
+//  PushAlarmModel.swift
+//  amatta
+//
+//  Created by 박준형 on 1/1/24.
+//
+
+import Foundation
+import SwiftUI
+
+class AlarmData: ObservableObject {
+    @Published var alarmName: String = ""
+    @Published var items: [Item] = []
+
+    struct Item {
+        var name: String
+        var importance: Float
+        var children: [Item]
+    }
+}
