7b6a30c18390315c5768503398b3a43cc872a986
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-03 11:53:53 +0900

    환경설정 페이지로 이동하기 위해 우선 SettingView 파일 생성

---
+++ amatta/SettingView.swift
@@ -0,0 +1,23 @@
+//
+//  SettingView.swift
+//  amatta
+//
+//  Created by 박준형 on 1/3/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct SettingView: View {
+    // 여기에 환경설정 뷰의 내용을 구현합니다.
+    var body: some View {
+        Text("환경설정")
+            // 다른 UI 요소와 로직을 추가할 수 있습니다.
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
