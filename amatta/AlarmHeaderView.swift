4ba533b285007a9996c9034befb32a3e1485bd18
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-04 12:26:05 +0900

    기종에 맞게 유동적인 레이아웃을 보여줘야 함

--- amatta/AlarmHeaderView.swift
+++ amatta/AlarmHeaderView.swift
@@ -9,16 +9,18 @@ import Foundation
 import SwiftUI
 
 struct AlarmHeaderView: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
     var body: some View {
         HStack {
             Text("알림 추가")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundColor(Color(red: 82 / 255, green: 182 / 255, blue: 154 / 255))
-                .padding(.leading, 45)
+                .padding(.leading, sizeClass == .compact ? 20 : 45)
             Spacer()
         }
         .padding(.top, 20)
+
     }
 }
 
