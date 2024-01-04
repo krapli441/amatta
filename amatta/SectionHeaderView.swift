ed95606700986310774f506525a19d51ace9918e
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-04 12:04:23 +0900

    기종마다 padding 변경하도록 수정해봄

--- amatta/SectionHeaderView.swift
+++ amatta/SectionHeaderView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct SectionHeaderView: View {
     let title: String
+    @Environment(\.horizontalSizeClass) var sizeClass
 
     var body: some View {
         HStack {
@@ -19,12 +20,13 @@ struct SectionHeaderView: View {
             Spacer()
         }
         .padding(.vertical, 5)
-        .padding(.leading, 45)
+        .padding(.leading, sizeClass == .compact ? 15 : 45)
     }
 }
 
 struct SectionHeaderView_Previews: PreviewProvider {
     static var previews: some View {
         SectionHeaderView(title: "섹션 제목")
+        .environment(\.horizontalSizeClass, .compact)
     }
 }
