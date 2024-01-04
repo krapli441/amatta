b1408017264cb993250c52f1c0b3b498625f98e5
박준형 <108733795+krapli441@users.noreply.github.com>
2024-01-04 14:29:25 +0900

    레이아웃 수정

--- amatta/AlarmEditAddItemView.swift
+++ amatta/AlarmEditAddItemView.swift
@@ -55,7 +55,7 @@ struct AlarmEditAddItemView: View {
                         }
                         .buttonStyle(ChoiceButtonStyle(isSelected: canContainOtherItems == false))
                     }
-                    .frame(maxWidth: UIScreen.main.bounds.width * 0.75)
+                    .frame(maxWidth: UIScreen.main.bounds.width * 0.83)
                     
                     if canContainOtherItems {
                     SectionHeaderView(title: "그 안에 무엇이 들어가나요?")
